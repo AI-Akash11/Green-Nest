306639c4b5e39c79d6ccbceebbdc58b330cd5129
Ali Imam Akash <akash1437aliimam@gmail.com>
2025-10-22 21:43:27 +0600

    added login page and app structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
+import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+    const links = <>
+    <NavLink className={'px-2 py-1 text-sm rounded-2xl font-semibold text-gray-600'} to={'/'}>Home</NavLink>
+    <NavLink className={'px-2 py-1 text-sm rounded-2xl font-semibold text-gray-600'} to={'/plants'}>Plants</NavLink>
+    <NavLink className={'px-2 py-1 text-sm rounded-2xl font-semibold text-gray-600'} to={'/myprofile'}>My Profile</NavLink>
+    </>
+  return (
+    <div className="navbar bg-base-100 shadow-sm md:px-10 lg:px-20">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <div
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {
+              links
+            }
+          </div>
         </div>
-    );
+        <div className="flex justify-start items-center">
+          <img className="w-8" src="https://i.ibb.co.com/dyt4mZT/plant-logo.png" alt="" />
+          <Link to={'/'} className="btn btn-ghost text-2xl text-yellow-400"><span className="text-green-500">Green</span>Nest</Link>
+        </div>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <div className="menu menu-horizontal space-x-4">
+          {
+            links
+          }
+        </div>
+      </div>
+      <div className="navbar-end">
+        <Link to={'/auth/login'} className="btn btn-success text-white">Login</Link>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [eye, setEye] = useState(true);
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-[#e0e5ec]">
+      <div className="bg-[#e0e5ec] p-8 rounded-2xl shadow-[9px_9px_16px_#a3b1c6,-9px_-9px_16px_#ffffff] w-[400px] md:w-[500px]">
+        <h2 className="text-center text-4xl font-semibold mb-15">Login</h2>
+
+        <form className="flex flex-col gap-4 relative">
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="px-3 py-2 rounded-xl bg-[#e0e5ec] shadow-inner shadow-[#a3b1c6]/70 outline-none"
+          />
+          <input
+            type={eye ? "password" : "text"}
+            name="password"
+            placeholder="Password"
+            className="px-3 py-2 rounded-xl bg-[#e0e5ec] shadow-inner shadow-[#a3b1c6]/70 outline-none"
+          />
+
+          <div
+            onClick={() => {
+              setEye(!eye);
+            }}
+            className="absolute right-4 top-17 text-xl cursor-pointer"
+          >
+            {eye ? <FaEyeSlash></FaEyeSlash> : <FaEye></FaEye>}
+          </div>
+
+          <button 
+          type="submit"
+          className="mt-8 w-full py-3 rounded-xl bg-[#e0e5ec] shadow-[5px_5px_10px_#a3b1c6,-5px_-5px_10px_#ffffff] font-semibold hover:bg-[#d1d9e6] transition">
+            Login
+          </button>
+        </form>
+
+        <div className="text-center mt-4 text-gray-600 text-sm">
+          Forgot your password?
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
